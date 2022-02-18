b94fee55499efece146e4dd273d9b5ba21389863
huangjianqin <18814127639@163.com>
2022-02-18 23:25:21 +0800

    java agent实现修改

---
+++ README.md
@@ -0,0 +1,2 @@
+# kin-java-agent
+java agent实现, 用于其他app attach该agent来, 使用该agent获取`Instrumentation`实例, 进而获取attach的app的jvm信息
--- src/main/java/org/kin/framework/hotswap/agent/JavaDynamicAgent.java
+++ src/main/java/org/kin/framework/hotswap/agent/JavaDynamicAgent.java
@@ -3,32 +3,36 @@ package org.kin.framework.hotswap.agent;
 import java.lang.instrument.Instrumentation;
 
 /**
- *
  * @author huangjianqin
  * @date 2018/2/3
- * 必须单独打包，并且MANNIFEST.MF设置
- * Agent-Class: JavaDynamicAgent.jar
- * Can-Redefine-Classes: true
  */
 public class JavaDynamicAgent {
+    /** {@link Instrumentation}实例 */
     private static Instrumentation instrumentation;
-    private static Object lockObject = new Object();
+    /** 充当锁的单例 */
+    private static final Object LOCK = new Object();
 
     /**
      * 方法必须叫agentmain
      */
     public static void agentmain(String args, Instrumentation inst) {
-        synchronized (lockObject) {
+        synchronized (LOCK) {
             if (instrumentation == null) {
                 instrumentation = inst;
             }
         }
     }
 
+    /**
+     * 获取{@link Instrumentation}实例
+     */
     public static Instrumentation getInstrumentation() {
         return instrumentation;
     }
 
+    /**
+     * 获取实例size
+     */
     public static long getObjectSize(Object o) {
         return instrumentation.getObjectSize(o);
     }
